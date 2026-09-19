566cf52559643b9e459e4adec860af3cc92a936c
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-09-19 12:42:51 +0530

    Zerobars Updated

--- nextpay/lib/main.dart
+++ nextpay/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nextpay/services/app_lock_service.dart';
+import 'package:zerobars/services/app_lock_service.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_provider.dart';
 import 'providers/theme_provider.dart';
--- nextpay/lib/screens/contact_history_screen.dart
+++ nextpay/lib/screens/contact_history_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nextpay/screens/payment_sheet_screen.dart';
+import 'package:zerobars/screens/payment_sheet_screen.dart';
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
 import '../providers/theme_provider.dart';
